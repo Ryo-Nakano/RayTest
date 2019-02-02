c18c02ca1ed6d8070d2313d5a9a949b8b70ed4de
Ryohei Nanano <matsuhara.9999ryo@gmail.com>
2019-02-02 23:14:03 +0900

    first

---
+++ Assets/Scripts/RayTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayTest : MonoBehaviour {
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		if(Input.GetMouseButtonDown(0))//左クリックされた時
+		{
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);//マウスの位置から飛ぶRayを作成→変数mouseRayに格納
+
+			RaycastHit hit;//Rayの衝突情報格納の為の変数を宣言(Raycasthit型)
+
+			if(Physics.Raycast(mouseRay, out hit))//mouseRayが何かに当たったら(衝突情報は変数hitの中)
+			{
+				Debug.Log("This is " + hit.collider.gameObject.name);
+			}
+		}
+	}
+}
